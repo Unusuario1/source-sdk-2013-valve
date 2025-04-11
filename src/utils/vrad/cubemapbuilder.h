68a6f921a5fb71f86923f5427cfeee9b28acdf60
Carlos suarez <110506367+Unusuario2@users.noreply.github.com>
2025-04-11 12:53:30 +0200

    .
    .

--- src/utils/vrad/cubemapbuilder.h
+++ src/utils/vrad/cubemapbuilder.h
@@ -1,16 +1,14 @@
-//========= Copyright Valve Corporation, All rights reserved. ============//
+//========= ------------------------------------------------------ ============//
 //
 // Purpose: 
 //
 // $NoKeywords: $
 //=============================================================================//
-
-
 #ifndef CUBEMAPBUILDER_H
 #define CUBEMAPBUILDER_H
 
 
-void BuildCubemaps(bool bHdrMode);
+void BuildCubemaps(const bool bHdrMode);
 
 
-#endif //CUBEMAPBUILDER_H
+#endif // CUBEMAPBUILDER_H
