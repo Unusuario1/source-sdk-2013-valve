3bb096f088e38030c032b280d78d8807227282ab
Carlos Suarez <110506367+Unusuario2@users.noreply.github.com>
2025-10-19 18:34:59 +0200

    a

--- src/utils/subrecteditor/coresubrecteditor.hpp
+++ src/utils/subrecteditor/coresubrecteditor.hpp
@@ -36,9 +36,14 @@
 #include <filesystem_tools.h>
 #include <resourcecopy/cresourcecopy.hpp>
 #include <KeyValues.h>
-#include "coresoundscapebase.hpp"
-#include "coresoundscaperule.hpp"
-#include "coresoundscaperootkv.hpp"
+#include <utlbuffer.h>
+#include <vector>
+
+
+//-----------------------------------------------------------------------------
+// Purpose:
+//-----------------------------------------------------------------------------
+using KvContainer = std::vector<KeyValues*>;
 
 
 //-----------------------------------------------------------------------------
@@ -47,9 +52,90 @@
 class CCoreSubRectEditor
 {
 private:
+	KeyValues*		m_pRootKv				= nullptr;
+	KvContainer*	m_pChildKvRentangle		= nullptr; // yeah i am not using KeyValues[][] :)
+	char			m_szFileName[MAX_PATH];
+
+
+private:
+	const int GetRectKvValues(const std::size_t uiPos, const char* pKeyName) const { m_pChildKvRentangle->at(uiPos)->GetInt(pKeyName); }
+	void SetRectKvValues(const uint16_t uiValue, const std::size_t uiPos, const char* pKeyName) const { m_pChildKvRentangle->at(uiPos)->SetInt(pKeyName, uiValue); }
 
 
 public:
+	//-----------------------------------------------------------------------------
+	// Purpose:
+	//-----------------------------------------------------------------------------
+	inline CCoreSubRectEditor::CCoreSubRectEditor(const char* pFileName)
+	{
+		V_sprintf_safe(m_szFileName, "%s.rect", pFileName);
+		m_pRootKv = new KeyValues("Rectangles");
+		m_pChildKvRentangle = new KvContainer();
+	}
 
 
+	//-----------------------------------------------------------------------------
+	// Purpose:
+	//-----------------------------------------------------------------------------
+	inline CCoreSubRectEditor::~CCoreSubRectEditor()
+	{
+		for (int i = 0; i < m_pChildKvRentangle->size(); ++i)
+			m_pChildKvRentangle->at(i)->deleteThis();
+		delete m_pChildKvRentangle;
+		delete[] m_szFileName;
+		m_pRootKv->deleteThis();
+	}
+
+
+	//-----------------------------------------------------------------------------
+	// Purpose:
+	//-----------------------------------------------------------------------------
+	inline int AddChildRectangle()
+	{
+		KeyValues* pChildKv = new KeyValues("rectangle");
+		pChildKv->CreateKey("min");
+		pChildKv->CreateKey("max");
+		
+		// Set the default values.
+		pChildKv->SetInt("min", 0);
+		pChildKv->SetInt("max", 0);
+		m_pChildKvRentangle->push_back(pChildKv);
+
+		// Add the last child as a subkey
+		m_pRootKv->AddSubKey(m_pChildKvRentangle->back());
+		return static_cast<int>(m_pChildKvRentangle->size() - 1);
+	}
+
+
+	//-----------------------------------------------------------------------------
+	// Purpose:
+	//-----------------------------------------------------------------------------
+	inline void RemoveChildRectangle(const std::size_t uiPos) { m_pRootKv->RemoveSubKey(m_pChildKvRentangle->at(uiPos)); }
+
+
+	//-----------------------------------------------------------------------------
+	// Purpose:
+	//-----------------------------------------------------------------------------
+	inline const std::size_t SizeOfChildKvList() { return m_pChildKvRentangle->size(); }
+	inline const std::size_t EndOfChildKvList() { return static_cast<int>(m_pChildKvRentangle->size() - 1); }
+
+
+	//-----------------------------------------------------------------------------
+	// Purpose:
+	//-----------------------------------------------------------------------------
+	inline const bool WriteRectFile() { return m_pRootKv->SaveToFile(g_pFileSystem, m_szFileName, "MOD"); }
+
+
+	//-----------------------------------------------------------------------------
+	// Purpose: Seters...
+	//-----------------------------------------------------------------------------
+	inline void SetMinKvValues(const uint16_t uiValue, const std::size_t uiPos) const { this->SetRectKvValues(uiValue, uiPos, "min"); }
+	inline void SetMaxKvValues(const uint16_t uiValue, const std::size_t uiPos) const { this->SetRectKvValues(uiValue, uiPos, "max"); }
+
+
+	//-----------------------------------------------------------------------------
+	// Purpose: Geters...
+	//-----------------------------------------------------------------------------
+	inline const int GetMinKvValues(const std::size_t uiPos) const { return this->GetRectKvValues(uiPos, "min"); }
+	inline const int GetMaxKvValues(const std::size_t uiPos) const { return this->GetRectKvValues(uiPos, "max"); }
 };
---
+++ src/utils/subrecteditor/subrecteditor.cpp
@@ -0,0 +1,83 @@
+#include "coresubrecteditor.hpp"
+#include <iostream>
+
+
+
+//-----------------------------------------------------------------------------
+// Purpose:   Parse command line
+//-----------------------------------------------------------------------------
+static void ParseCommandline(int argc, char* argv[])
+{
+    for (int i = 1; i < argc; ++i)
+    {
+        if (!V_stricmp(argv[i], "-FullMinidumps"))
+        {
+            EnableFullMinidumps(true);
+        }
+        else if (!V_stricmp(argv[i], "-game") || !V_stricmp(argv[i], "-vproject"))
+        {
+            if (++i < argc && argv[i][0] != '-')
+            {
+                char* gamePath = argv[i];
+                if (!gamePath)
+                {
+                    Error("\nError: \'-game\' requires a valid path argument. NULL path\n");
+                }
+                V_strcpy_safe(gamedir, gamePath);
+            }
+            else
+            {
+                Error("\nError: \'-game\' requires a valid path argument.\n");
+            }
+        }
+    }
+}
+
+
+
+//-----------------------------------------------------------------------------
+// Purpose:
+//-----------------------------------------------------------------------------
+static void Init(int argc, char* argv[])
+{
+    SetupDefaultToolsMinidumpHandler();
+    CommandLine()->CreateCmdLine(argc, argv);
+    InstallSpewFunction();
+    ParseCommandline(argc, argv);
+
+    CmdLib_InitFileSystem(gamedir);
+}
+
+
+//-----------------------------------------------------------------------------
+// Purpose:
+//-----------------------------------------------------------------------------
+static void Destroy(int argc, char* argv[])
+{
+    DeleteCmdLine(argc, argv);
+    CmdLib_Cleanup();
+    CmdLib_Exit(0);
+}
+
+
+int main(int argc, char* argv[])
+
+{
+    Init(argc, argv);
+
+    CCoreSubRectEditor* aa = new CCoreSubRectEditor("test");
+
+
+    int pos = aa->AddChildRectangle();
+    aa->SetMinKvValues(0, pos);
+    aa->SetMaxKvValues(256, pos);
+    
+    pos = aa->AddChildRectangle();
+    aa->SetMinKvValues(0, pos);
+    aa->SetMaxKvValues(256, pos);
+    aa->RemoveChildRectangle(pos);
+    aa->WriteRectFile();
+
+
+    Destroy(argc, argv);
+}
